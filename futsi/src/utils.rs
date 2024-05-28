a9409f063325648b0f811cbcca417a5204c46acf
Akash Singh <aki24695@gmail.com>
2024-05-28 13:19:44 +0530

    added basic variable support

--- futsi/src/utils.rs
+++ futsi/src/utils.rs
@@ -1,12 +1,9 @@
 pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
-    let digits_end = s
-        .char_indices()
-        .find_map(|(idx, c)| if c.is_ascii_digit() { None } else { Some(idx) })
-        .unwrap_or_else(|| s.len());
+    take_while(|c| c.is_ascii_digit(), s)
+}
 
-    let digits = &s[..digits_end];
-    let remainder = &s[digits_end..];
-    (remainder, digits)
+pub(crate) fn extract_whitespace(s: &str) -> (&str, &str) {
+    take_while(|c| c == ' ', s)
 }
 
 pub(crate) fn extract_op(s: &str) -> (&str, &str) {
@@ -16,6 +13,31 @@ pub(crate) fn extract_op(s: &str) -> (&str, &str) {
     }
 
     (&s[1..], &s[0..1])
+}
+
+pub(crate) fn take_while(accept: impl Fn(char) -> bool, s: &str) -> (&str, &str) {
+    let extracted_end = s
+        .char_indices()
+        .find_map(|(idx, c)| if accept(c) { None } else { Some(idx) })
+        .unwrap_or_else(|| s.len());
+
+    let extracted = &s[..extracted_end];
+    let remainder = &s[extracted_end..];
+    (remainder, extracted)
+}
+
+pub(crate) fn extract_ident(s: &str) -> (&str, &str) {
+    let input_starts_with_alphabetic = s
+        .chars()
+        .next()
+        .map(|c| c.is_ascii_alphabetic())
+        .unwrap_or(false);
+
+    if input_starts_with_alphabetic {
+        take_while(|c| c.is_ascii_alphanumeric(), s)
+    } else {
+        (s, "")
+    }
 }
 
 
@@ -61,5 +83,15 @@ mod tests {
     #[test]
     fn extract_slash() {
         assert_eq!(extract_op("/4"), ("4", "/"));
+    }
+
+    #[test]
+    fn extract_spaces() {
+        assert_eq!(extract_whitespace("    1"), ("1", "    "));
+    }
+
+    #[test]
+    fn cannot_extract_ident_beginning_with_number() {
+        assert_eq!(extract_ident("123abc"), ("123abc", ""));
     }
 }
